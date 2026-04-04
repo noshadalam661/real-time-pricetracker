14ed7e03248a2a901d3a902e57a8f75261677ff9
noshadalam66 <alam.noshad70@gmail.com>
2026-04-04 13:36:08 +0300

    repositories created with websocket. added functions to connect, disconnect and send price update

--- app/src/main/java/com/real_time_price_tracker/data/repository/StockRepository.kt
+++ app/src/main/java/com/real_time_price_tracker/data/repository/StockRepository.kt
@@ -1,6 +1,47 @@
 package com.real_time_price_tracker.data.repository
-
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import okhttp3.*
 
 class StockRepository {
+    private val client = OkHttpClient()
+    private var webSocket: WebSocket? = null
 
+    private val _priceUpdates = MutableSharedFlow<String>(extraBufferCapacity = 64)
+    val priceUpdates = _priceUpdates.asSharedFlow()
+
+    private val _isConnected = MutableStateFlow(false)
+    val isConnected = _isConnected.asStateFlow()
+
+    fun connect() {
+        val request = Request.Builder().url("wss://ws.postman-echo.com/raw").build()
+        webSocket = client.newWebSocket(request, object : WebSocketListener() {
+            override fun onOpen(webSocket: WebSocket, response: Response) {
+                _isConnected.value = true
+            }
+            override fun onMessage(webSocket: WebSocket, text: String) {
+                CoroutineScope(Dispatchers.IO).launch { _priceUpdates.emit(text) }
+            }
+            override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
+                _isConnected.value = false
+            }
+            override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+                _isConnected.value = false
+            }
+        })
+    }
+
+    fun sendUpdate(message: String) {
+        webSocket?.send(message)
+    }
+
+    fun disconnect() {
+        webSocket?.close(1000, "User stopped feed")
+        _isConnected.value = false
+    }
 }
