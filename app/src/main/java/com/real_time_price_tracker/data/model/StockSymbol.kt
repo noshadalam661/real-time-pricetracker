0806d02360e68de4b86f7209f5639752b9a1fe36
noshadalam66 <alam.noshad70@gmail.com>
2026-04-04 13:31:12 +0300

    stock symbol model added

--- app/src/main/java/com/real_time_price_tracker/data/model/StockSymbol.kt
+++ app/src/main/java/com/real_time_price_tracker/data/model/StockSymbol.kt
@@ -1,5 +1,18 @@
 package com.real_time_price_tracker.data.model
 
-class StockSymbol{
+import androidx.compose.ui.graphics.Color
 
+data class StockSymbol(
+    val symbol: String,
+    val price: Double = 0.0,
+    val prevPrice: Double = 0.0,
+    val description: String = "Leading global company in the technology and services sector."
+) {
+    val trend: Trend = when {
+        price > prevPrice -> Trend.UP
+        price < prevPrice -> Trend.DOWN
+        else -> Trend.NEUTRAL
+    }
 }
+
+enum class Trend { UP, DOWN, NEUTRAL }
